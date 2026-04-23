58daa1813005b739496239d14d31166a0f70c8f4
Pratishtha <nandwalpratishtha06@gmail.com>
2026-04-23 15:26:38 +0530

    New changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './index.css'
 import {BrowserRouter , Route, Routes} from 'react-router-dom'
-import Login from './pages/login';
-import Register from './pages/Register';
-import Home from './pages/home';
-import Profile from './pages/Profile';
+import { lazy,Suspense } from 'react';
+
+const Login = lazy(()=>import ('./pages/login'));
+const Register = lazy(()=>import ('./pages/Register'));
+const Home = lazy(()=>import ('./pages/home'));
+const Profile = lazy(()=>import ('./pages/Profile'));
+const UploadResources = lazy(()=>import( './pages/UploadResources'));
+
+
 import Layout from './components/Layout';
-import UploadResources from './pages/UploadResources';
 import UserContextProvider from './components/UserContext';
 import axios from 'axios';
 import AuthGate from './components/AuthGate';
@@ -21,11 +25,13 @@ function App() {
       <AuthGate>
        <Routes>
         <Route element={<Layout/>}>
+         <Suspense fallback={<div>Loading...</div>}>
           <Route index element = {<Home/>}/>
           <Route path='/register' element = {<Register/>}/>
           <Route path='/login' element = {<Login/>}/>         
           <Route path='/my-profile' element={<Profile/>}/>
           <Route path='/upload-resource' element={<UploadResources/>}/>
+         </Suspense>
         </Route>
        </Routes>
       </AuthGate>
